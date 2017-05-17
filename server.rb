da24bb6d38e4a998b99b0f39dd442ed35ae7dadf
rduquesnoy <rduquesnoy@drwholdings.com>
2017-05-17 10:07:41 -0500

    roll dice, show details, process. this is for GET request response

--- server.rb
+++ server.rb
@@ -1,46 +1,43 @@
 require 'socket' 
-#require 'uri'
+require 'cgi'
 
 server = TCPServer.new 12345
 
-while session = server.accept
-  request = session.gets
-  puts request
+while socket = server.accept
+  request = socket.gets
   method, full_path = request.split(' ')
+  params = {}
   if full_path.include? '?'
     path, queries = full_path.split('?')
-  else
-    path = full_path
-    queries = None
+    params = CGI::parse(queries)
   end
 
   response = "Hello World! #{Time.now}\n"
-  #puts URI.parse(request).query
-  if queries.include? '&'
-    query_array = queries.split('&')
-  else
-    query_array = [queries]
-  end
-
-  query_array.times do |query|
-    puts query
-  end
-
-  if params['dice']
+  if params.key?('dice') and params['dice']
     total = 0
-    num = params['dice'].to_i
-    num.times do |count|
-      total += 1 + rand(6)
+    details = ""
+    num = params['dice'][0].to_i
+    if num == 0
+        response = "You need to specify a number greater than zero\n"
+    else
+      num.times do |count|
+        roll = 1 + rand(6)
+        total += roll
+        details << "[#{roll}] "
+      end
+      response = "I rolled a D6 #{num} times and totaled #{total}\n"
+      if params.key?('details') and params['details'][0].downcase == "true"
+        response << "Individual rolls: #{details}\n" 
+      end
     end
-    response = "I rolled a D6 #{num} times and totaled #{total}\n" 
   end
 
-  session.print "HTTP/1.1 200 OK\r\n" +
+  socket.print "HTTP/1.1 200 OK\r\n" +
                "Content-Type: text/plain\r\n" +
                "Content-Length: #{response.bytesize}\r\n" +
                "Connection: close\r\n"
 
-  session.print "\r\n"
-  session.print response
-  session.close
+  socket.print "\r\n"
+  socket.print response
+  socket.close
 end
